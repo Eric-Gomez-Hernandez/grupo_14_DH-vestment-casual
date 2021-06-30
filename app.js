65ff782ee24dfa34c51f7b814610edfa0af675ce
adrian-cr <acr77frb@gmail.com>
2021-06-30 18:07:04 -0500

    added product editing pages

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const methodOverride=require('method-override');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -15,30 +15,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
+app.listen(3000,()=>{console.log("Servidor corriendo en el puerto 3000")});
+
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+const adminRouter = require('./routes/admin');
 
 
-var indexRouter = require('./routes/index');
-var UserRouter = require('./routes/user');
 app.use('/', indexRouter);
-app.use('/categorias', indexRouter);
-app.use('/detalle-producto', indexRouter);
-app.use('/carrito', indexRouter);
-app.use('/registro', UserRouter);
+app.use('/user', userRouter);
+app.use('/admin', adminRouter);
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function(req, res, next) {next(createError(404))});
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-
-module.exports = app;
