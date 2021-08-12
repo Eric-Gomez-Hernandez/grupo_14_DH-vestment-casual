d0e7cf5d6ff28777259ef8b2a6b19df5aa68ff15
Eric Gómez <egomez_99@yahoo.com>
2021-08-11 23:19:15 -0500

    reg-ses-cook

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const { body } = require('express-validator');
 const userController = require('../controllers/userController');
+const guestMiddleware = require('../middlewares/guestMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/registro', userController.registro)
-router.get('/profile', userController.profileAccess)
-router.post('/login', userController.loginAccess)
+router.get('/registro',guestMiddleware, userController.registro);
+router.post('/registro', userController.crearRegistro);
+router.post('/login', userController.loginAccess);
+router.get('/profile',authMiddleware,userController.profileAccess);
+router.get('/logout',userController.loginOut);
+
 
 module.exports = router;
